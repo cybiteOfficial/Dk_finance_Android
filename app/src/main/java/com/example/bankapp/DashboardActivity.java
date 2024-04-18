afa5aa117dfa54b70806d822bdd67a04086206c5
panik-79 <pujanparekh2609@gmail.com>
2024-04-18 12:19:31 +0530

    dashboard ui implemented

--- app/src/main/java/com/example/bankapp/DashboardActivity.java
+++ app/src/main/java/com/example/bankapp/DashboardActivity.java
@@ -3,55 +3,60 @@ package com.example.bankapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
-import android.widget.Button;
+import android.widget.TextView;
+import android.view.Gravity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class DashboardActivity extends AppCompatActivity {
-    private Button newRegistrationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        newRegistrationButton = findViewById(R.id.new_reg_btn);
-        ImageView userProfileImageView = findViewById(R.id.user_profile);
 
+        LinearLayout newRegistrationButton = findViewById(R.id.open_account_btn);
+        LinearLayout approvedLoansButton = findViewById(R.id.approved_loans_btn);
+        LinearLayout rejectedLoansButton = findViewById(R.id.rejected_loans_btn);
+        LinearLayout failedPaymentsButton = findViewById(R.id.failed_payments_btn);
 
         newRegistrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start NewRegistrationActivity
                 Intent intent = new Intent(DashboardActivity.this, NewRegistrationActivity.class);
                 startActivity(intent);
             }
         });
-
-        // Set OnClickListener for the user profile ImageView.
-        userProfileImageView.setOnClickListener(new View.OnClickListener() {
+        approvedLoansButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Show the user profile dropdown menu
-                showPopupMenu(v);
+                // Start ApprovedLoansActivity
+                Intent intent = new Intent(DashboardActivity.this, ApprovedLoansActivity.class);
+                startActivity(intent);
             }
         });
-
-        // Add OnClickListener for other buttons if needed
+        rejectedLoansButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start RejectedLoansActivity
+                Intent intent = new Intent(DashboardActivity.this, RejectedLoansActivity.class);
+                startActivity(intent);
+            }
+        });
+        failedPaymentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start FailedPaymentsActivity
+                Intent intent = new Intent(DashboardActivity.this, FailedPaymentsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private void showPopupMenu(View v) {
-        PopupMenu popupMenu = new PopupMenu(this, v);
-        popupMenu.inflate(R.menu.user_profile_menu); // Inflate menu resource
 
-        // Set name and ID directly as menu item titles
-        popupMenu.getMenu().findItem(R.id.username).setTitle("Name: SampleUser");
-        popupMenu.getMenu().findItem(R.id.employee_id).setTitle("Employee ID: 123456");
-
-
-
-
-        popupMenu.show();
-    }
 }
